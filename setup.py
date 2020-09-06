f3534d22ab007682eb8f6d166e295e29a5b5fa17
JT W <jwolohan@gmail.com>
2020-09-06 09:14:57 -0400

    ADD details to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,19 @@ with open("README.md", "r") as f
   long_description = f.read()
 
 setuptools.setup(
+    name="fncore",
+    version="2020.9",
+    author="JT Wolohan",
+    author_email="jwolohan@gmail.com",
+    description="Fuctional programming core library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jtwool/fncore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
